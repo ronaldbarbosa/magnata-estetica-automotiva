ef8ef267f5765ce40b74f6f0ad0901cd2a36cca3
Ronald Barbosa <ronaldverissimo@hotmail.com>
2023-06-09 21:15:32 -0300

    Implementada exclusao de video no Cloudinary

--- servicos/signals.py
+++ servicos/signals.py
@@ -1,5 +1,6 @@
 import cloudinary.api
 from django.db.models.signals import pre_delete
+from cloudinary_storage.storage import RawMediaCloudinaryStorage
 from django.dispatch import receiver
 
 
@@ -10,8 +11,10 @@ def excluir_img_cloudinary(sender, instance, **kwargs):
             chave_publica_imagem = instance.imagem.public_id
             cloudinary.api.delete_resources(chave_publica_imagem)
 
+@receiver(pre_delete)
+def excluir_video_cloudinary(sender, instance, **kwargs):
+    if sender.__name__ == 'Servico':
         if instance.video:
             chave_publica_video = instance.video.public_id
-            cloudinary.api.delete_resources(chave_publica_video)
-
-        # Todo: exclusão do video não funcionou
+            storage = RawMediaCloudinaryStorage(resource_type='video')
+            storage.delete(chave_publica_video)
